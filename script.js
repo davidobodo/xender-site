a3e93e1cbadaca5902ea25d9334496339641b968
David Obodo <obododavid5@gmail.com>
2019-09-15 01:24:10 +0100

    changing images from page one to page two successful

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-const moveDown = document.querySelector('.one_next')
+const moveDown = document.querySelector('.one_next');
 
-moveDown.addEventListener('click', function(){
-    document.querySelector('main').classList.add('viewing-page-2')
-})
+moveDown.addEventListener('click', nextPage);
+
+function nextPage (){
+    document.querySelector('main').style.transform = 'translate3d(0px, -100vh, 0px)';
+    document.querySelector('.sec1').classList.remove('activeImage');
+    document.querySelector('.sec2').classList.add('activeImage');
+    // viewingPagetwo()
+}
+
+function viewingPagetwo (){
+    document.querySelector('.slide_container').classList.add('viewing-page-2');    
+}
