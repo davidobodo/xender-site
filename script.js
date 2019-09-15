c985e78b583e55047d4347c556584add0e0ee12f
David Obodo <obododavid5@gmail.com>
2019-09-15 11:31:14 +0100

    second image in page two being outoutted correctly

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ function nextPage (){
     document.querySelector('main').style.transform = 'translate3d(0px, -100vh, 0px)';
     document.querySelector('.sec1').classList.remove('activeImage');
     document.querySelector('.sec2').classList.add('activeImage');
-    document.querySelector('.slide_container').style.top = '160%';
+    document.querySelector('.slide_container').style.top = '110%';
     window.setTimeout(function moveside(){
-        document.querySelector('.slide_container').style.marginLeft = '-400px'
+        document.querySelector('.slide_container').style.marginLeft = '-500px';
+        document.querySelector('#two .img_container').style.marginLeft = '180px'
+        document.querySelector('#two .img_container').style.opacity = '1'
     }, 1000)
 }
 
